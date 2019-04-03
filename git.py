440815a5a8603eb2a3f6977cd08b84ff624fc2f2
sbukirwa <sbukirwa@usiu.ac.ke>
2019-04-04 01:37:08 +0300

    None

--- git.py
+++ git.py
@@ -5,7 +5,7 @@ def run():
     """To run Github code"""
     msg = Popen("git add .", shell=True)
     commit = input("What change did you make Sonia? :  ")
-    msg = Popen("git commit -m "+str(commit), shell=True)
+    msg = Popen("git commit -m '"+str(commit)+"'", shell=True)
     msg = Popen("git push origin master", shell=True)
 
 
